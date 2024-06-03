333e84b49702c3c8ea277f7fef8d3dea1f707695
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-06-03 17:23:05 -0300

    chore: Update menu component to use dynamic links and highlight active link

--- app/_components/menu.tsx
+++ app/_components/menu.tsx
@@ -1,14 +1,15 @@
+"use client";
+
 import {
   BarChartBigIcon,
   Calendar,
   Dumbbell,
-  GithubIcon,
   Home,
-  Instagram,
   MenuIcon,
   MessageCircleIcon,
 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import { cn } from "../_lib/utils";
 
 interface MenuProps {
@@ -16,50 +17,32 @@ interface MenuProps {
 }
 
 const Menu = ({ className }: MenuProps) => {
+  const pathname = usePathname();
+
+  const links = [
+    { href: "/", icon: Home, label: "Início" },
+    { href: "/pages/all-exercises", icon: Dumbbell, label: "Treinos" },
+    { href: "/pages/graphics", icon: BarChartBigIcon, label: "Gráficos" },
+    { href: "/pages/mensages", icon: MessageCircleIcon, label: "Mensagens" },
+    { href: "/pages/calendar", icon: Calendar, label: "Calendário" },
+    { href: "/pages/more", icon: MenuIcon, label: "Mais" }
+  ];
+
   return (
     <div className={cn("flex px-8 pt-6 pb-10 w-[100vw] bg-menu justify-between rounded-lg", className)}>
-      <Link href={"/"} className="flex flex-col items-center gap-2">
-        <Home size={20} />
-        <span className="text-xs">Início</span>
-      </Link>
-      <Link href={"/pages/all-exercises"} className="flex flex-col items-center gap-2">
-        <Dumbbell size={20} />
-        <span className="text-xs">Treinos</span>
-      </Link>
-      <Link href={"/pages/graphics"} className="flex flex-col items-center gap-2">
-        <BarChartBigIcon size={20} />
-        <span className="text-xs">Gráficos</span>
-      </Link>
-      <Link href={"/pages/mensages"} className="flex flex-col items-center gap-2">
-        <MessageCircleIcon size={20} />
-        <span className="text-xs">Mensagens</span>
-      </Link>
-      <Link href={"/pages/calendar"} className="flex flex-col items-center gap-2">
-        <Calendar size={20} />
-        <span className="text-xs">Calendário</span>
-      </Link>
-      <Link href={"/"} className="flex flex-col items-center gap-2">
-        <MenuIcon size={20} />
-        <span className="text-xs">Mais</span>
-      </Link>
-
-      {/* <Link
-        href={"https://github.com/m4rrec0s/tasklist-app"}
-        target="_blank"
-        className="flex items-center rounded-lg border border-white gap-2 max-w-fit p-2"
-      >
-        <GithubIcon size={20} />
-        <span className="text-sm">Github</span>
-      </Link>
-
-      <Link
-        href={"https://www.instagram.com/marcos_henrique_eu/"}
-        target="_blank"
-        className="flex items-center rounded-lg border border-white gap-2 max-w-fit p-2"
-      >
-        <Instagram size={20} />
-        <span className="text-sm">Instagram</span>
-      </Link> */}
+      {links.map((link) => {
+        const Icon = link.icon;
+        const isActive = pathname === link.href;
+        return (
+          <Link href={link.href} key={link.href} className={cn("flex flex-col items-center gap-2", {
+            "opacity-100": isActive,
+            "opacity-50": !isActive
+          })}>
+            <Icon size={20} />
+            <span className="text-xs">{link.label}</span>
+          </Link>
+        );
+      })}
     </div>
   );
 };
