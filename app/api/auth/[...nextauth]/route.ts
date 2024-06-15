72fb20a38b33a421a316d8060d7f1413c2c7bbfc
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-06-15 12:55:46 -0300

    feat: Update NextAuth configuration to include secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,7 +11,8 @@ const handler = NextAuth({
         clientId: process.env.GOOGLE_CLIENT_ID as string,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ]
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
