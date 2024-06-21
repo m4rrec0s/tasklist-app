b097ada6cb8cbbc6fe77fb977dd3e5057aecc044
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2024-06-21 13:02:59 -0300

    feat: Create a initial screen component to include TaskList logo and Google sign-in button

---
+++ app/_components/initial-screen.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+import { ChromeIcon } from "lucide-react";
+import Link from "next/link";
+
+const InitialScreen = () => {
+  const { data } = useSession();
+
+  return (
+    <>
+      {!data?.user ? (
+        <main className="absolute bottom-0 p-8 left-0 h-[100vh] w-[100vw] bg-menu z-10 flex items-center justify-center">
+          <div className="flex flex-col items-center">
+            <Image
+              className="w-[230px] h-[230px]"
+              src="/TaskList-logo.png"
+              alt="TaskList logo"
+              width={230}
+              height={230}
+            />
+            <h1 className="font-bold text-xl">Bem vindo ao TaskList</h1>
+            <p className="font-normal text-sm text-muted">
+              Seu melhor companheiro de academia!
+            </p>
+            <div className="flex flex-col text-white font-semibold mt-8 h-52 w-full">
+              <Button
+                className="w-full p-8 rounded-lg bg-transparent border border-muted-foreground gap-2 hover:bg-gray-500"
+                onClick={() => signIn()}
+              >
+                <ChromeIcon className="w-6 h-6 mr-2" />
+                <span>Entrar com Google</span>
+              </Button>
+            </div>
+          </div>
+          <div className="absolute bottom-5">
+            <Link className="text-sm font-semibold underline" href="https://github.com/m4rrec0s">By @m4rrec0s</Link>
+          </div>
+        </main>
+      ) : null}
+    </>
+  );
+};
+
+export default InitialScreen;
